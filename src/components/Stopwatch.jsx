5c4c5e73dfa4f7fc5c115702e369402c7a837c52
Asutosh Sahoo <asutosh.98.sahoo@gmail.com>
2022-06-17 22:50:02 +0530

    done

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -22,6 +22,7 @@ export const Stopwatch = () => {
         <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
         
         <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        
         <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
       </h1>
       <br />
