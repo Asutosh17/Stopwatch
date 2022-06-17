463e6b515a78f7070507d073a77004541c01ebb8
Asutosh Sahoo <asutosh.98.sahoo@gmail.com>
2022-06-17 21:14:52 +0530

    done

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -1,36 +1,35 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 export const Stopwatch = () => {
+  const [time, setTime] = useState(0);
+  const [stopwatch, setStopwatch] = useState(false);
 
-    const [time, setTime]  = useState(0)
-    const [stopwatch, setStopwatch]  = useState(false)
-    
-    useEffect(() => {
-        let interval = null;
-        if(stopwatch){
-            interval = setInterval(() => {
-                setTime(prev => prev+10)
-            },10)
-        }else{
-            clearInterval(interval)
-        }
-        return () => clearInterval(interval)
-    },[stopwatch])
+  useEffect(() => {
+    let interval = null;
+    if (stopwatch) {
+      interval = setInterval(() => {
+        setTime(prev => prev + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [stopwatch]);
 
   return (
     <div>
-        <h1>
-        <span>{("0" + Math.floor((time/60000)%60)).slice(-2)}:</span>
-        <span>{("0" + Math.floor((time/1000)%60)).slice(-2)}:</span>
-        <span>{("0" + ((time/10)%100)).slice(-2)}</span>
-        </h1>
-        <br />
-        <div>
-            <button onClick={() => setStopwatch(true)}>Start</button>
-            <button onClick={() => setStopwatch(false)}>Stop</button>
-            <button onClick={() => setStopwatch(true)}>Pause</button>
-            <button onClick={() => setTime(0)}>Reset</button>
-        </div>
+      <h1>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+      </h1>
+      <br />
+      <div>
+        <button onClick={() => setStopwatch(true)}>Start</button>
+        <button onClick={() => setStopwatch(false)}>Stop</button>
+        <button onClick={() => setStopwatch(true)}>Pause</button>
+        <button onClick={() => setTime(0)}>Reset</button>
+      </div>
     </div>
-  )
-}
+  );
+};
